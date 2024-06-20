7e5bdb3f40097b91d559cb515908e6591f4dc131
suraj chauhan <100895916+sonubintw@users.noreply.github.com>
2024-06-20 16:42:51 +0530

    added new homePage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RedirectComponent from './RedirectComponent';
+import Home from './home';
 
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/EProcess" element={<RedirectComponent />} />
-
       </Routes>
     </Router>
   );
